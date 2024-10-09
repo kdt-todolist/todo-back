526c937778f9d1d0e7452eb408c3576e8e2887c2
subinggrae <subin000602@gmail.com>
2024-10-09 14:02:09 +0900

    fix: dotenv.config()에 옵션 부여
    app.js와 .env가 같은 폴더에 존재하지 않아
    path 옵션을 주어서 이를 해결

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: './src/config/.env' });
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DATABASE,
   connectionLimit: process.env.DB_CONNECTION,
   port: process.env.DB_PORT
 });
