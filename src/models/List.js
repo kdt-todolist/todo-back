f5c86b663747368f18a2e033445df0ac112f0c55
shhwang0522 <shhwangofficial@gmail.com>
2024-10-13 16:00:42 +0900

    fix: update 로직 수정, userId 반영, bulk 추가

--- src/models/List.js
+++ src/models/List.js
@@ -1,25 +1,16 @@
 const pool = require("../config/db");
 
-const createList = async (title) => {
-  const sql = `INSERT INTO lists (title) VALUES (?)`;
-  let fields = [title];
+const createList = async (userId, title) => {
+  const sql = `INSERT INTO lists (user_id, title) VALUES (?, ?)`;
+  let fields = [userId, title];
 
   let [result] = await pool.query(sql, fields);
   return result;
 };
 
-const updateListTitleById = async (id, title) => {
-  const sql = `UPDATE lists SET title = ? WHERE id = ?`;
-  let fields = [title, id];
-  let [result] = await pool.query(sql, fields);
-
-  return result.affectedRows;
-};
-
-const updateListIsVisibleById = async (id, isVisible) => {
-  const sql = `UPDATE lists SET is_visible = ? WHERE id = ?`;
-
-  let fields = [isVisible, id];
+const updateListById = async (id, title, isVisible) => {
+  const sql = `UPDATE lists SET title = ?, is_visible = ? WHERE id = ?`;
+  let fields = [title, isVisible, id];
   let [result] = await pool.query(sql, fields);
 
   return result.affectedRows;
@@ -33,16 +24,27 @@ const deleteListById = async (id) => {
   return result.affectedRows;
 };
 
-const findAllLists = async () => {
-  const sql = `SELECT * FROM lists`;
-  let [result] = await pool.query(sql);
+const findAllLists = async (userId) => {
+  const sql = `SELECT * FROM lists WHERE user_id = ?`;
+  let fields = [userId];
+  let [result] = await pool.query(sql, fields);
   return result;
 };
 
+const createBulkList = async (userId, lists) => {
+  const sql = `INSERT INTO lists (user_id, title, is_visible) VALUES ?`;
+  let fields = [];
+  lists.forEach(list => {
+    fields.push([userId, list.title, list.isVisible]);
+  });
+  let [result] = await pool.query(sql, [fields]);
+  return result;
+}
+
 module.exports = {
   createList,
-  updateListIsVisibleById,
-  updateListTitleById,
+  updateListById,
   deleteListById,
   findAllLists,
+  createBulkList
 };
